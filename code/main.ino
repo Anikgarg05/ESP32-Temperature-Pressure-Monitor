0d9c997bf5b321c7a47a4d48a8d247c26506f7fb
Anik_garg <garganik05@gmail.com>
2026-05-08 18:04:32 +0530

    Update main.ino

--- code/main.ino
+++ code/main.ino
@@ -1,1 +1,21 @@
+#include <Wire.h>
+#include <Adafruit_BMP280.h>
 
+Adafruit_BMP280 bmp;
+
+void setup() {
+  Serial.begin(115200);
+  bmp.begin();
+}
+
+void loop() {
+  Serial.print("Temp: ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" °C");
+
+  Serial.print("Pressure: ");
+  Serial.print(bmp.readPressure() / 100.0F);
+  Serial.println(" hPa");
+
+  delay(2000);
+}
